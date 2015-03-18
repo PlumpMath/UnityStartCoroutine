53e854174267987822f28a476905922136f6181b
ms <ms.shao@dygame.cn>
2015-03-18 17:13:38 +0800

    20150318:create repository and commit

---
+++ Assets/Scripts/CalledMethodBringer.cs
@@ -0,0 +1,95 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;//for Debug.Log
+using System.Diagnostics;//for StackFrame,StackTrace
+using System.Reflection;//for MethodBase
+
+public class CalledMethodBringer : MonoBehaviour
+{
+    protected static CalledMethodBringer Self;
+    // Use this for initialization
+    void Start()
+    {
+        Self = this;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    //Instance
+    public static CalledMethodBringer Instance()
+    {
+        return Self;
+    }
+    //To get callers:
+    //被呼叫者 CalledMethod
+    //呼叫者   Caller
+    public void CalledMethod(bool IsSingle)
+    {
+        if (IsSingle)
+        {
+            string currentName = new StackTrace(true).GetFrame(0).GetMethod().Name;
+            string callerName = new StackTrace(true).GetFrame(1).GetMethod().Name;
+            UnityEngine.Debug.Log("caller Name: " + callerName);//it is Load()
+            UnityEngine.Debug.Log("current Name: " + currentName);//it is TestMethod()
+        }
+        else
+        {
+            StackTrace trace = new StackTrace();
+            StackFrame[] frames = trace.GetFrames();            
+            for (int i = 0; i < trace.FrameCount; i++)
+            {
+                UnityEngine.Debug.Log("The Caller Name" + i + "=" + frames[i].GetMethod().DeclaringType.Name + "." + frames[i].GetMethod().Name + "()" );
+            }
+        }
+    }
+    //Sample:
+    void Sample()
+    {
+        TestMethod();
+    }
+    //
+    void TestMethod()
+    {
+        string currentName = new StackTrace(true).GetFrame(0).GetMethod().Name;
+        string callerName = new StackTrace(true).GetFrame(1).GetMethod().Name;
+        UnityEngine.Debug.Log("current Line: " + new StackTrace(true).GetFrame(0).GetFileLineNumber().ToString());//it is new StackTrace() line
+        UnityEngine.Debug.Log("caller Method: " + callerName);//it is Sample()
+        UnityEngine.Debug.Log("current Method: " + currentName);//it is TestMethod()
+    }
+    //StackTrace for Exception
+    void ExceptionMethod()
+    {
+        string s = "1234";
+        try
+        {
+            int iR = Convert.ToInt32(s);
+            iR++;
+        }
+        catch (Exception e)
+        {
+            StackTrace trace = new StackTrace(e);
+            StackFrame[] frames = trace.GetFrames();
+            for (int i = 0; i < trace.FrameCount; i++)
+            {
+                UnityEngine.Debug.Log("The Caller Name " + i + "=" + frames[i].GetMethod().Name);
+            }
+        }
+        //Result:
+        //The 0 Caller: ParseInt32
+        //The 1 Caller: ToInt32
+        //The 2 Caller: ExceptionMethod
+    }
+    //
+    void TestClassName()
+    {
+        StackTrace trace = new StackTrace(true);
+        //取得呼叫當前方法之上一層類別(GetFrame(1))的屬性
+        MethodBase mb = trace.GetFrame(1).GetMethod();
+        //取得呼叫當前方法之上一層類別(父方)的取得宣告這個成員的類別。 (繼承自 MemberInfo)。
+        string showString1 = mb.DeclaringType.Name ;
+        //取得呼叫當前方法之上一層類別(父方)的取得宣告這個成員的類別。 (繼承自 MemberInfo)。
+        string showString2 = mb.DeclaringType.FullName ;    
+    }
+}
---
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,252 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour 
+{
+    //Final Result:
+    void Result()
+    {
+        /*
+         * Start1
+         * init1
+         * Start2
+         * Start3
+         * Run1
+         * Start4
+         * Start5
+         * Test1
+         * Load1
+         * Start6
+         * Start7
+         * read1
+         * read3
+         * read5
+         * Start8
+         * Start9
+         * Pass1
+         * Pass2
+         * StartA
+         * StartB
+         * //Loop Start
+         * StartC
+         * StartD
+         * loop1
+         * StartE
+         * StartF
+         * StartC
+         * StartD
+         * loop1
+         * StartE
+         * StartF
+         * ...
+         * //LoopEnd
+         * caller = CalledMethodBringer.CalledMethod()
+         * caller = BewBehaviourScript.Start()
+         * StartG
+         * StartH
+         * Run2
+         * Run3
+         * Load2
+         * Load3
+         * Test2
+         * Test3
+         * read6
+         * read7
+         * Pass3
+         * Pass4
+         * init4
+         * //another loop Start
+         * loop2
+         * loop3
+         * loop2
+         * loop3
+         * loop2
+         * loop3
+         * ...
+         * //another loop End
+         * Run4
+         * Run5
+         * //another loop Start
+         * loop4
+         * loop5
+         * loop4
+         * loop5
+         * loop4
+         * loop5
+         * ...
+         * loop6
+         * loop7
+         * loop6
+         * loop7
+         * loop6
+         * loop7
+         * ...
+         * //another loop End
+         * Init2
+         * Init5
+         * Init3
+         * Init6
+         * init4
+         * init7
+         * Pass5
+         * Pass6
+         */
+    }
+	// Use this for initialization
+	void Start () 
+    {
+        Debug.Log("start1");
+        //test yield wait second
+        StartCoroutine(Init(1));        
+        Debug.Log("start2");
+        Debug.Log("start3");
+        //test yield wait null
+        StartCoroutine(Run());
+        Debug.Log("start4");
+        Debug.Log("start5");
+        //test function call another StartCoroutine
+        StartCoroutine(Test());
+        Debug.Log("start6");
+        Debug.Log("start7");
+        //test www
+        StartCoroutine(Read());
+        Debug.Log("start8");
+        Debug.Log("start9");
+        //test function call another yield wait second
+        StartCoroutine(Pass());
+        Debug.Log("startA");
+        Debug.Log("startB");
+        //test for loop 
+        bool IsTimeout = true;
+        float fTimeout = Time.time + 3.0f;//test 3 second        
+        int iCount = 0;
+        while (IsTimeout)
+        {
+          //yield return new WaitForSeconds(1.0f);//每隔一秒//這種做法才有效果,先call函式之後再call yield會無限call StartCoroutine,導致unity掛
+            Debug.Log("startC");
+            Debug.Log("startD");
+            StartCoroutine(Loop());//這種做法無法做到等1秒
+            Debug.Log("startE");
+            Debug.Log("startF");
+           	//after test , break loop
+            if (Time.time > fTimeout)
+            {
+                IsTimeout = false;
+            }
+            //use count to break loop
+            iCount++;
+            if (iCount > 5) IsTimeout = false;
+        }
+        //print Caller function name
+        CalledMethodBringer.Instance().CalledMethod(false);
+        Debug.Log("startG");
+        Debug.Log("startH");
+	}	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+    //
+    IEnumerator Run()
+    {
+        Debug.Log("Run1");
+        yield return null;
+        Debug.Log("Run2");
+        Debug.Log("Run3");
+        yield return null;
+        Debug.Log("Run4");
+        Debug.Log("Run5");
+    }
+    //
+    IEnumerator Init(int iInput)
+    {
+		string s = "init"+(iInput+0) ;
+		Debug.Log(s);
+        yield return StartCoroutine(init1());
+		s = "init"+(iInput+1) ;
+        Debug.Log(s);
+        yield return StartCoroutine(init2());
+		s = "init"+(iInput+2) ;
+		Debug.Log(s);
+        yield return StartCoroutine(init3());
+		s = "init"+(iInput+3) ;
+		Debug.Log(s);
+    }
+    IEnumerator init1()
+    {
+        // 模擬初始化
+        yield return new WaitForSeconds(2);//
+    }
+    IEnumerator init2()
+    {
+        // do somthing..
+        yield return new WaitForSeconds(2);//
+    }
+    IEnumerator init3()
+    {
+        // do somthing..
+        yield return new WaitForSeconds(2);//
+    }
+    IEnumerator Test()
+    {
+        Debug.Log("test1");
+        yield return StartCoroutine(Load());
+        Debug.Log("test2");
+        Debug.Log("test3");
+    }
+    IEnumerator Load()
+    {
+        Debug.Log("load 1");
+        yield return null;
+        Debug.Log("load 2");
+        Debug.Log("load 3");
+    }
+    IEnumerator Read()
+    {
+        Debug.Log("read 1");
+        WWW www = new WWW("www.youcannotpass.com.tw");
+        if (www.isDone)
+        {
+            Debug.Log("read 2 Done");//error will also done? seem is not
+        }
+        //
+        if (string.IsNullOrEmpty(www.error))
+        {
+            Debug.Log("read 3 Error");
+        }
+        else
+        {
+            Debug.Log("read 4 Done and Finish");
+        }
+        Debug.Log("read 5");
+        yield return www;   
+        Debug.Log("read 6");
+        Debug.Log("read 7");
+    }
+    IEnumerator Pass()
+    {
+        Debug.Log("Pass 1");
+        Debug.Log("Pass 2");
+        yield return 0;
+        Debug.Log("Pass 3");
+        Debug.Log("Pass 4");
+		yield return StartCoroutine(Init(4));
+		Debug.Log("Pass 5");
+		Debug.Log("Pass 6");
+    }
+    IEnumerator Loop()
+    {
+        Debug.Log("loop 1");
+        yield return null;
+        Debug.Log("loop 2");
+        Debug.Log("loop 3");
+        yield return new WaitForSeconds(1.0f);
+        Debug.Log("loop 4");
+        Debug.Log("loop 5");
+        yield return null;
+        Debug.Log("loop 6");
+        Debug.Log("loop 7");
+    }
+    
+}
